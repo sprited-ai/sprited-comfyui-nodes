d748afd3924e7066afc5e62e6e0c8daf15e87b70
Sprited Dev <dev@sprited.app>
2025-08-01 12:54:54 -0700

    scaffolding

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for sprited_nodes."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """sprited-comfyui-nodes"""
+__email__ = "dev@sprited.app"
+__version__ = "0.0.1"
+
+from .src.sprited_nodes.nodes import NODE_CLASS_MAPPINGS
+from .src.sprited_nodes.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
