611d7d2ba4e9f5025dccee2dd85293b3cc23d465
Candle <2665740+kndlt@users.noreply.github.com>
2026-01-14 01:28:21 -0800

    Fix: Remove stale import of deleted extract_loop module

--- src/sprited_nodes/__init__.py
+++ src/sprited_nodes/__init__.py
@@ -1,6 +1,5 @@
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 from .download_video import VideoDownloader
-from .extract_loop import LoopTrimNode
 from .slice import SliceBatch, SliceLatents
 
-__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "VideoDownloader", "LoopTrimNode", "SliceBatch", "SliceLatents"]
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "VideoDownloader", "SliceBatch", "SliceLatents"]
